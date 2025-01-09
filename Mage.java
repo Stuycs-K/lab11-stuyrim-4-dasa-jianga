62cc75f864f0c3dce9f6bd3e8f1f9c1c19403c4d
Amrit Das <amritdas8015@gmail.com>
2025-01-09 10:59:16 -0500

    started creating variables for mage

--- Mage.java
+++ Mage.java
@@ -1,3 +1,21 @@
 public abstract class Mage extends Adventurer{
-
+  public Mage(String name){
+    super(name);
+  }
+  public Mage(String name, int hp){
+    super(name, hp);
+  }
+  private int mana, manaMax;
+  public String getSpecialName(){
+    return "Earthquake";
+  }
+  public int getSpecial(){
+    this.mana;
+  }
+  public int getSpecialMax(){
+    return 50;
+  }
+  public void setSpecial(int n){
+    if(n < )
+  }
 }
