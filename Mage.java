cba67bb879bf8b1f2acf426e5f2c3c52113563f7
Amrit Das <amritdas8015@gmail.com>
2025-01-10 10:44:41 -0500

    wrote first attack method

--- Mage.java
+++ Mage.java
@@ -1,21 +1,27 @@
+import.java.util.*;
 public abstract class Mage extends Adventurer{
+  private int mana, manaMax;
   public Mage(String name){
     super(name);
+    this.mana = 25;
+    this.manaMax = 50;
   }
   public Mage(String name, int hp){
     super(name, hp);
+    this.mana = 25;
+    this.manaMax = 50;
   }
-  private int mana, manaMax;
   public String getSpecialName(){
     return "Mana";
   }
   public int getSpecial(){
     this.mana;
   }
+
   public int getSpecialMax(){
-    this.manaMax = 50;
-    return 50;
+    return manaMax;
   }
+
   public void setSpecial(int n){
     if(n < this.manaMax){
       this.mana = n;
@@ -24,7 +30,20 @@ public abstract class Mage extends Adventurer{
       this.mana = this.manaMax;
     }
   }
-  
+
+  public void applyDamage(int amount){
+    this.HP -= amount;
+  }
+
+  public String attack(Adventurer other){
+    int damage = 4;
+    restoreSpecial(7);
+    other.applyDamage(damage);
+    return this.getName() + " attacked" + other + " and dealt" + damage + " points of damage";
+    if(other.getHP() <= 0){
+      restoreSpecial(this.manaMax);
+    }
+  }
   public String specialAttack(ArrayList<Adventurer> party){
 
   }
